4255303ed24ba532588265806c36357333a780b4
pilliwickle <kurkovvalera@gmail.com>
2023-04-13 14:02:55 +0300

    add title list button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {TaskType, Todolist } from './Todolist';
 
 function App() {
+
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "CSS", isDone: true},
+    { id: 2, title: "JS", isDone: true},
+    { id: 3, title: "React", isDone: false},
+  ]
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Her", isDone: true},
+    { id: 2, title: "Mad Max", isDone: true},
+    { id: 3, title: "3", isDone: false},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title="what to learn" tasks={tasks1}/>
+      <Todolist title="Movie" tasks={tasks2}/>
     </div>
   );
 }
+
 
 export default App;
